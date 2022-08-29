41ff224e6ee92fbb960d0794429afdc6e87afa55
Leonardo Tricotti <tricottileo@gmail.com>
2022-08-29 20:53:28 -0300

    seguimos corrigiendo

--- js/extracciones.js
+++ js/extracciones.js
@@ -1,5 +1,5 @@
 //Variable que recupera la informacion del local storage
-let saldoCajaOperable = localStorage.getItem(saldo);
+let saldoCajaOperable = localStorage.getItem("saldo");
 //Funcion que convierte el dato recuperado del localstorage a numero
 const convertirStorageANumero = () => parseFloat(saldoCajaOperable);
 //Codigo que captura el boton que confirma la operacion
@@ -30,16 +30,9 @@ const actualizarSaldoStorage = () =>
     actualizarSaldoCajaAhorro()
   ));
 //Funcion que convierte al formato de moneda local el dato parseado
-const numeroADinero = () => numeroAPesos(extraerDinero());
+const numeroAPesos = () => numeroADinero(extraerDinero());
 //Codigo que convierte al formato de moneda local el saldo simulado
-const convertirSaldoADinero = () => numeroAPesos(actualizarSaldoCajaAhorro());
-//Funcion que coinvierte un numero al formato de pesos argentinos
-const numeroAPesos = (dinero) => {
-  return (dinero = new Intl.NumberFormat("es-AR", {
-    style: "currency",
-    currency: "ARS",
-  }).format(dinero));
-}
+const convertirSaldoADinero = () => numeroADinero(actualizarSaldoCajaAhorro());
 //Funcion que captura la informacion brindada por el usuario y la convierte en un objeto
 captura.onclick = () => {
   // Constructor del objeto depositos;
@@ -57,14 +50,11 @@ captura.onclick = () => {
     capturarDiaExtraccion(),
     capturarHoraExtraccion(),
     nombrarOperacion(),
-    numeroADinero(),
+    numeroAPesos(),
     convertirSaldoADinero(),
   );
   //Llamada a las funciones declaradas 
   confirmarOperacion();
-  modificarOpcion();
-  agregarTexto();
-  modificarOpcion();
   actualizarSaldoStorage();
 };
 //Codigo que dispara un alerta que confirma o cancela la operación
@@ -72,7 +62,7 @@ const text = document.querySelector(".text");
 const confirmarOperacion = () => {
   Swal.fire({
     icon: "question",
-    title: `Desea extraer la suma de ${numeroADinero()} ?`,
+    title: `Desea extraer la suma de ${numeroAPesos()} ?`,
     confirmButtonText: 'Save',
     confirmButtonColor: "#3085d6",
     confirmButtonText: "Aceptar",
@@ -102,23 +92,5 @@ const confirmarOperacion = () => {
 clean.onclick = () => {
   inputExtraccion.value = "";
  }
-//Funcion que modifica el HTML al momento de devolver la operacion solicitada por el usuario
-function agregarTexto() {
-  //Codigo que agrega texto al html
-  let textoAgregado = document.querySelector(".agregar-texto");
-  textoAgregado.innerText = "Desea realizar otra operacion?";
-}
-//Funcion que modifica el HTML al momento de devolver la operacion solicitada por el usuario
-function modificarOpcion() {
-  //Codigo que cambia texto del html
-  let opcionModificada = document.querySelector(".opcion-modificada");
-  opcionModificada.innerHTML = "";
-  opcionModificada.innerHTML =
-    "<p>Si</p> <a href='../cajero/cajero.html'> <div class='btn-derecha' id='btn-saldo'></div></a></li>";
-  //Codigo que cambia texto del html
-  let opcionModificadaDos = document.querySelector(".opcion-modificada-dos");
-  opcionModificadaDos.innerHTML = "";
-  opcionModificadaDos.innerHTML =
-    '<p>No</p> <a href="../salir/salir.html" class="link"> <div class="btn-derecha"></div></a>';
-}
+
 
